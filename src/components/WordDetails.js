f64fd0851bf4164fec095d4edc670bdcec31ce19
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-10-04 19:07:55 +0530

    output displaying improved

--- src/components/WordDetails.js
+++ src/components/WordDetails.js
@@ -34,8 +34,72 @@ const { wordDetails, loading } = useSelector((state) => state.word);
   }
 
   return (
-    <div className="word-details">
-      <h2>{wordDetails[0].word}</h2>
+    <div className="WordDetails">
+      {
+        wordDetails.map((obj)=>{
+            let wordName = obj.word;
+
+            
+            return(
+                <div>
+                    <h2>{obj.word}</h2>
+                    {
+                        obj.phonetics.map((phone)=>{
+                            return(
+                                <div>
+                                    <p><i>{phone.text}</i></p>
+                                    <audio controls>
+                                        <source src={phone.audio} type="audio/mpeg"/>
+                                    </audio>
+                                    
+                                </div>
+                            )
+                        })
+
+                    }
+                    {
+                        obj.meanings.map((meaning)=>{
+                            return (
+                                <div>
+                                    <strong>{meaning.partOfSpeech}</strong>
+                                    {
+                                        
+                                        meaning.definitions.map((defObj)=>{
+                                            console.log(defObj.synonyms);
+                                            console.log(defObj.antonyms);
+                                            return(
+                                                <div>
+                                                    <p>{defObj.definition}</p>
+                                                    {defObj.synonyms.length>0?<p>Synonyms:</p>:""}
+                                                    {
+                                                    
+                                                        defObj.synonyms.map((synonym)=>{
+                                                            return(
+                                                                <p>{synonym}, </p>
+                                                            )
+                                                        })
+                                                    }
+                                                    {defObj.antonyms.length>0?<p>Antonyms:</p>:""}
+                                                    {
+                                                        defObj.antonyms.map((antonym)=>{
+                                                            return (
+                                                                <p>{antonym}, </p>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            )
+
+        })
+      }
       {/* Render other word details like phonetics, meanings, etc. */}
     </div>
   );
